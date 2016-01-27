6e458da4ea76210e48b64928dc9c5940f66d95a6
rohini.joshi <rohini.joshi@raweng.com>
2016-01-27 15:32:39 +0530

    add code validations in server.js

--- adaptor/server.js
+++ adaptor/server.js
@@ -5,7 +5,6 @@ var mongoose   = require("mongoose");
 var cors 			 = require('cors');
 var Model 		 = require("./schema").Model;
 var userHelper = require("./user-helper");
-
 //app.use(cors());
 
 mongoose.connect('mongodb://localhost/urlShortner')
@@ -29,20 +28,22 @@ app.use("*",function(req, res, next) {
 
 app.get('/url', function (req, res) {
 	var random = userHelper.randomString(6,'abktldmghncdew')
-		console.log("req------",req.query.url)
-	var snip   = new Model({
-		code :random,
-		url  :req.query.url
-	});
-	
+
 	Model.findOne({url:req.query.url}, function(err, data){
 		if(data){
 			res.send("http://localhost:9001/"+data.code);
 		} else{
-			snip.save(function(err, snip) {
-			  if (err) return console.error(err);
-				res.send("http://localhost:9001/"+random);
-			});
+			getCode(random)
+			.then(function(code){
+				var snip   = new Model({
+					code :code,
+					url  :req.query.url
+				});
+				snip.save(function(err, snip) {
+				  if (err) return console.error(err)
+					res.send("http://localhost:9001/"+code)
+				});
+			})
 		}
   });
 });
@@ -63,3 +64,16 @@ app.get('/:code',function(req, res){
 app.listen(9001, function () {
   console.log('Snipper app listening on port 9001!');
 });
+
+function getCode(code){
+	return new Promise(function (resolve, reject){
+	  Model.findOne({code: code}, function(err, data){
+			if(data){
+				var random = userHelper.randomString(6,'abktldmghncdew')
+			  getCode(random)
+			}else{
+				resolve(code)
+			}
+	  });
+	})
+}
